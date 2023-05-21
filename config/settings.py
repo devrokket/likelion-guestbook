927e46ea6534e96ce3cd9015f00b1860bd11745f
devrokket <rokany@cau.ac.kr>
2023-05-21 10:46:53 +0900

    fix: cors

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://bangmyeonglock.kro.kr:8000",
-    "http://likelion-guest.kro.kr",
+    "http://likelion-guestbook.kro.kr",
 ]
 
 ROOT_URLCONF = 'config.urls'
